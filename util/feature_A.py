e2de6376e1492b8a32952ac2aa76ec629575cddf
Oliverdron <oliver.gyimothy@icloud.com>
2025-04-02 14:00:18 +0200

    chosen feature A functions

---
+++ util/feature_A.py
@@ -0,0 +1,95 @@
+import cv2
+import numpy as np
+from math import sqrt, floor, ceil, nan, pi
+from skimage import color, exposure
+from skimage.color import rgb2gray
+from skimage.feature import blob_log
+from skimage.filters import threshold_otsu
+from skimage.measure import label, regionprops
+from skimage.transform import resize
+from skimage.transform import rotate
+from skimage import morphology
+from sklearn.cluster import KMeans
+from skimage.segmentation import slic
+from skimage.color import rgb2hsv
+from scipy.stats import circmean, circvar, circstd
+from statistics import variance, stdev
+from scipy.spatial import ConvexHull
+
+
+
+def mean_asymmetry(mask, rotations = 30):
+    
+    asymmetry_scores = rotation_asymmetry(mask, rotations)
+    mean_score = sum(asymmetry_scores.values()) / len(asymmetry_scores)
+
+    return mean_score          
+
+def rotation_asymmetry(mask, n: int):
+
+    asymmetry_scores = {}
+
+    for i in range(n):
+
+        degrees = 90 * i / n
+
+        rotated_mask = rotate(mask, degrees)
+        cutted_mask = cut_mask(rotated_mask)
+
+        asymmetry_scores[degrees] = asymmetry(cutted_mask)
+
+    return asymmetry_scores
+
+def asymmetry(mask):
+    
+
+    row_mid, col_mid = find_midpoint_v1(mask)
+
+    upper_half = mask[:ceil(row_mid), :]
+    lower_half = mask[floor(row_mid):, :]
+    left_half = mask[:, :ceil(col_mid)]
+    right_half = mask[:, floor(col_mid):]
+
+    flipped_lower = np.flip(lower_half, axis=0)
+    flipped_right = np.flip(right_half, axis=1)
+
+    hori_xor_area = np.logical_xor(upper_half, flipped_lower)
+    vert_xor_area = np.logical_xor(left_half, flipped_right)
+
+    total_pxls = np.sum(mask)
+    hori_asymmetry_pxls = np.sum(hori_xor_area)
+    vert_asymmetry_pxls = np.sum(vert_xor_area)
+
+    asymmetry_score = (hori_asymmetry_pxls + vert_asymmetry_pxls) / (total_pxls * 2)
+
+    return round(asymmetry_score, 4)
+
+def cut_mask(mask):
+    
+    col_sums = np.sum(mask, axis=0)
+    row_sums = np.sum(mask, axis=1)
+
+    active_cols = []
+    for index, col_sum in enumerate(col_sums):
+        if col_sum != 0:
+            active_cols.append(index)
+
+    active_rows = []
+    for index, row_sum in enumerate(row_sums):
+        if row_sum != 0:
+            active_rows.append(index)
+
+    col_min = active_cols[0]
+    col_max = active_cols[-1]
+    row_min = active_rows[0]
+    row_max = active_rows[-1]
+
+    cut_mask_ = mask[row_min:row_max+1, col_min:col_max+1]
+
+    return cut_mask_
+
+def find_midpoint_v1(image):
+    
+    row_mid = image.shape[0] / 2
+    col_mid = image.shape[1] / 2
+    return row_mid, col_mid
