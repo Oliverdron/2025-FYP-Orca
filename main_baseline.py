b58ac72e8e61410ef66bf1636b297d698c9876e6
Rudrakau <Realrudrakaushik@gmail.com>
2025-04-23 11:45:22 +0200

    Update main_baseline.py
    Planning for baseline

---
+++ main_baseline.py
@@ -0,0 +1,5 @@
+# pull in the main functions from each of the features and iterate through them 
+# to create a baseline model
+# Feature A : mean_asymmetry()
+
+# Pseudocode: Read File, Check with Masks, incorporate features - 
