46bb17c4e14d03260463940aba627a3bf31bb1a8
Oliverdron <oliver.gyimothy@icloud.com>
2025-05-23 17:50:51 +0200

    Take out  the not meaningful plots

--- util/data_analysis.py
+++ util/data_analysis.py
@@ -1,3 +1,6 @@
+#our model is mainly trained on middle aged patients
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
@@ -9,7 +12,8 @@ df = pd.read_csv(metadata_path)
 
 
 # Plot histograms for numeric columns
-numeric_cols = df.select_dtypes(include=['number']).columns
+numeric_cols = df.select_dtypes(include=['number']).columns.drop('lesion_id', errors='ignore')
+
 for col in numeric_cols:
     plt.figure()
     df[col].hist()
@@ -19,11 +23,14 @@ for col in numeric_cols:
     plt.tight_layout()
     plt.show()
 
-# Correlation matrix
-corr = df.corr(numeric_only=True)
+# Compute correlation matrix, excluding specified non-meaningful columns
+numeric_df = df.select_dtypes(include=['number']).drop(['biopsed', 'lesion_id'], axis=1, errors='ignore')
+corr = numeric_df.corr()
+
+# Plot correlation matrix
 plt.figure()
 plt.imshow(corr, aspect='auto')
-plt.title('Correlation Matrix')
+plt.title('Correlation Matrix (excluding biopsed and lesion_id)')
 plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
 plt.yticks(range(len(corr.index)), corr.index)
 plt.colorbar()
